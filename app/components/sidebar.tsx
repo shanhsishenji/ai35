b74f31bcf1938a54d08f093042a6b860f31f953a
Marilynn Paranada <129602245+shanhsishenji@users.noreply.github.com>
2023-09-28 21:55:29 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,7 +5,7 @@ import styles from "./home.module.scss";
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
 import GithubIcon from "../icons/qq.svg";
-import ChatGptIcon from "../icons/ai.svg";
+import ChatGptIcon from "../icons/ms.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
 import MaskIcon from "../icons/mask.svg";
